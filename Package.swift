090b2221022fa50b4c372576d3df02b5676ae206
Douglas Silva <silvadouglasw@gmail.com>
2021-08-04 18:03:25 -0300

    second commit

--- Package.swift
+++ Package.swift
@@ -13,14 +13,18 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/dji-sdk/Mobile-SDK-iOS", .branch("master")),
+//        .package(url: "https://github.com/dji-sdk/Mobile-SDK-iOS", .branch("master")),
     ],
     targets: [
-        .target(
-            name: "SiteScanSwiftPackage",
-            dependencies: ["DJISDK"]),
-        .testTarget(
-            name: "SiteScanSwiftPackageTests",
-            dependencies: ["SiteScanSwiftPackage"]),
+        .binaryTarget(name: "SiteScanSwiftPackage",
+                      path: "https://s3.amazonaws.com/sitescan-ios-dependencies/djisdk/DJISDK.framework_4.11.zip")
+//        .target(
+//            name: "SiteScanSwiftPackage",
+//            dependencies: ["DJISDK"]),
+//        .testTarget(
+//            name: "SiteScanSwiftPackageTests",
+//            dependencies: ["SiteScanSwiftPackage"]),
     ]
 )
+
+
