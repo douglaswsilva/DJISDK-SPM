c84ebd46b4272e2dca9e5be5f3d16bff8aa6a227
Douglas Silva <silvadouglasw@gmail.com>
2021-08-05 15:38:12 -0300

    Modify package file to match Dima's structure.

--- Package.swift
+++ Package.swift
@@ -6,21 +6,11 @@ import PackageDescription
 let package = Package(
     name: "SiteScanSwiftPackage",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SiteScanSwiftPackage",
-            targets: ["SiteScanSwiftPackage"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+            targets: ["DJISDK"]),
     ],
     targets: [
-        .target(
-            name: "SiteScanSwiftPackage",
-            dependencies: ["DJISDK"]),
-        .testTarget(
-            name: "SiteScanSwiftPackageTests",
-            dependencies: ["SiteScanSwiftPackage", "DJISDK"]),
         .binaryTarget(
            name: "DJISDK",
            url: "https://github.com/douglaswsilva/DJISDK-SPM/releases/download/0.1.2/DJISDK.xcframework.zip",
